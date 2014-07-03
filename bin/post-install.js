6774a27fc3dc57e41145bf4513942c1f8ff1e55f
Adam Miller <adam@flingo.tv>
2014-07-02 21:54:14 -0700

    removed bower components, upgraded postinstall script to actually work

--- bin/post-install.js
+++ bin/post-install.js
@@ -1,21 +1,32 @@
 #!/usr/bin/env node
 
-// We need to build handlebars to generate its compiler/parser.js file
-var exec = require('child_process').exec;
-var path = require('path');
-var fs = require('fs');
-var filepath = path.join(path.dirname(fs.realpathSync(__filename)), '..');
+var path = require('path'),
+    fs = require('fs'),
+    filepath = path.join(path.dirname(fs.realpathSync(__filename)), '..'),
+    util  = require('util'),
+    spawn = require('child_process').spawn;
 
-// TODO: this is dumb...fix this.
-// Install bower deps, build handlebars to generate its parser.js file, build our project.
-exec( 'bower install && cd ' + filepath + '/bower_components/handlebars && npm install && grunt build && cd ../../ && grunt build', function (error, stdout, stderr) {
-  if (stderr !== null) {
-    console.log('' + stderr);
-  }
-  if (stdout !== null) {
-    console.log('' + stdout);
-  }
-  if (error !== null) {
-    console.log('' + error);
-  }
-});
+// Install Rebound's bower dependancies
+function installBowerDeps(){
+  ps = spawn('bower', ['install'], {stdio: "inherit", cwd: filepath});
+  ps.on('exit', npmInstallHandlebars);
+}
+
+// npm install our handlebars dependancy
+function npmInstallHandlebars(){
+  ps = spawn('npm', ['install'], {stdio: "inherit", cwd: filepath + '/bower_components/handlebars'});
+  ps.on('exit', gruntBuildHandlebars);
+}
+
+// Build handlebars to generate parser.js file
+function gruntBuildHandlebars(){
+  ps = spawn('grunt', ['build'], {stdio: "inherit", cwd: filepath + '/bower_components/handlebars'});
+  ps.on('exit', gruntBuildRebound);
+}
+
+// Build our project
+function gruntBuildRebound(){
+  ps = spawn('grunt', ['build'], {stdio: "inherit", cwd: filepath});
+}
+
+installBowerDeps();
--- bower_components/htmlbars/packages/morph/lib/main.js
+++
@@ -1,5 +0,0 @@
-import Morph from "morph/morph";
-export var Morph;
-
-import DOMHelper from "morph/dom-helper";
-export var DOMHelper;
