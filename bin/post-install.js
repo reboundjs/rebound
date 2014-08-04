3b5de18201ab3a60185f14a2c6c8b0b506199540
Adam Miller <adam@pages.me>
2014-08-04 13:34:06 -0700

    Streamlined npm install process. Added extra debug output to postinstall

--- bin/post-install.js
+++ bin/post-install.js
@@ -8,37 +8,43 @@ var path = require('path'),
 
 // Install Rebound's bower dependancies
 function installBowerDeps(){
+  console.log('REBOUND POSTINSTALL: Installing Rebound Bower Deps...')
   ps = spawn('bower', ['install'], {stdio: "inherit", cwd: filepath});
   ps.on('exit', npmInstallHandlebars);
   ps.on('error', function (err) {
-    console.log('Error Running Bower Install In Rebound Post Install:', err);
+    console.log('REBOUND POSTINSTALL: Error Running Rebound Bower Install!', err);
   })
 }
 
 // npm install our handlebars dependancy
 function npmInstallHandlebars(){
+  console.log('REBOUND POSTINSTALL: Rebound Bower Install Success!')
+  console.log('REBOUND POSTINSTALL: Installing Handlebars NPM Deps...')
   ps = spawn('npm', ['install'], {stdio: "inherit", cwd: filepath + '/bower_components/handlebars'});
   ps.on('exit', gruntBuildHandlebars);
   ps.on('error', function (err) {
-    console.log('Error Running NPM Install In Rebound Post Install:', err);
+    console.log('REBOUND POSTINSTALL: Error Running Handlebars NPM Install!', err);
   })
 }
 
 // Build handlebars to generate parser.js file
 function gruntBuildHandlebars(){
+  console.log('REBOUND POSTINSTALL: Handlebars NPM Install Success!')
+  console.log('REBOUND POSTINSTALL: Building Handlebars...')
   ps = spawn('grunt', ['build'], {stdio: "inherit", cwd: filepath + '/bower_components/handlebars'});
   ps.on('exit', gruntBuildRebound);
   ps.on('error', function (err) {
-    console.log('Error Running Grunt Build Handlebars In Rebound Post Install:', err);
+    console.log('REBOUND POSTINSTALL: Error Running Handlebars Grunt Build!', err);
   })
 }
 
 // Build our project
 function gruntBuildRebound(){
-  console.log("Starting Grunt Build");
+  console.log('REBOUND POSTINSTALL: Handlebars Grunt Build Success!')
+  console.log('REBOUND POSTINSTALL: Running Rebound Grunt Build...')
   ps = spawn('grunt', ['build'], {stdio: "inherit", cwd: filepath});
   ps.on('error', function (err) {
-    console.log('Error Running Grunt Build Rebound In Rebound Post Install:', err);
+    console.log('REBOUND POSTINSTALL: Error Running Rebound Grunt Build!', err);
   })
 }
 
