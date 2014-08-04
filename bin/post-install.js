cf8136ca81cd39fbe8624c19511f2df4d25863af
Adam Miller <adam@pages.me>
2014-08-04 13:07:14 -0700

    HTMLbars link should be a git url...

--- bin/post-install.js
+++ bin/post-install.js
@@ -10,18 +10,27 @@ var path = require('path'),
 function installBowerDeps(){
   ps = spawn('bower', ['install'], {stdio: "inherit", cwd: filepath});
   ps.on('exit', npmInstallHandlebars);
+  ps.on('error', function (err) {
+    console.log('Error Running Bower Install In Rebound Post Install:', err);
+  })
 }
 
 // npm install our handlebars dependancy
 function npmInstallHandlebars(){
   ps = spawn('npm', ['install'], {stdio: "inherit", cwd: filepath + '/bower_components/handlebars'});
   ps.on('exit', gruntBuildHandlebars);
+  ps.on('error', function (err) {
+    console.log('Error Running NPM Install In Rebound Post Install:', err);
+  })
 }
 
 // Build handlebars to generate parser.js file
 function gruntBuildHandlebars(){
   ps = spawn('grunt', ['build'], {stdio: "inherit", cwd: filepath + '/bower_components/handlebars'});
   ps.on('exit', gruntBuildRebound);
+  ps.on('error', function (err) {
+    console.log('Error Running Grunt Build Handlebars In Rebound Post Install:', err);
+  })
 }
 
 // Build our project
@@ -29,7 +38,7 @@ function gruntBuildRebound(){
   console.log("Starting Grunt Build");
   ps = spawn('grunt', ['build'], {stdio: "inherit", cwd: filepath});
   ps.on('error', function (err) {
-    console.log('Error Running Grunt Build:', err);
+    console.log('Error Running Grunt Build Rebound In Rebound Post Install:', err);
   })
 }
 
