e524584e9d32e4d2bcb0f4a3f378234cb201d514
Adam Miller <adam@pages.me>
2015-01-25 22:42:28 -0800

    Removing /dist directory

--- dist/cjs/rebound-compiler/rebound-compiler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequire = function (obj) {
-  return obj && (obj["default"] || obj);
-};
-
-var compile = _interopRequire(require("rebound-compiler/compiler"));
-
-exports.compile = compile;
