af0918799b35b23a6f71bad1cc98d3f786e06816
Adam Miller <adam@pages.me>
2015-06-14 19:21:26 -0700

    Added codeclimate.yml and cleaned up old unneeded directories

--- bin/compile.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-var path = require('path');
-var fs = require('fs');
-var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../dist/commonjs/rebound/compiler.js');
-var Rebound = require(lib);
-if(typeof process.argv[4] === 'string') process.argv[4] = JSON.parse(process.argv[4]);
-console.log(Rebound.precompile(process.argv[2], process.argv[3], process.argv[4]));
-return;
--- bin/start-test-server.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-var grunt = require('grunt').tasks(['start']);
