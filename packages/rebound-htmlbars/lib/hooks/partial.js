2b369a9cc291a9e3ca08edfd4413b74d5e57fd83
Adam Miller <ammiller@linkedin.com>
2016-02-06 19:25:29 -0800

    Fix for dynamic lazy partials

--- packages/rebound-htmlbars/lib/hooks/partial.js
+++ packages/rebound-htmlbars/lib/hooks/partial.js
@@ -1,5 +1,6 @@
 import $ from "rebound-utils/rebound-utils";
 import loader from "rebound-router/loader";
+import LazyValue from "rebound-htmlbars/lazy-value";
 
 var PARTIALS = {};
 
@@ -21,9 +22,9 @@ export function registerPartial(name, template){
 export default function partial(renderNode, env, scope, path){
 
   // If no path is passed, yell
-  if(!path){ console.error('Partial hook must be passed path!'); }
+  if(!path){ console.error('Partial helper must be passed a path!'); }
 
-  // Resolve the value of path
+  // Resolve our path value
   path = path.isLazyValue ? path.value : path;
 
   // Create new child scope for partial
@@ -31,19 +32,27 @@ export default function partial(renderNode, env, scope, path){
 
   var render = this.buildRenderResult;
 
+  // Because of how htmlbars works with re-renders, we need a contextual element
+  // for our partial that will not disappear on it when lazy partials are loaded.
+  // We use a `<rebound-partial>` element for this.
+  var node = document.createElement('rebound-partial');
+  node.setAttribute('path', path);
+
   // If a partial is registered with this path name, render it
   if(PARTIALS[path] && !Array.isArray(PARTIALS[path])){
-    return render(PARTIALS[path], env, scope, { contextualElement: renderNode}).fragment;
+    node.appendChild(render(PARTIALS[path], env, scope, { contextualElement: renderNode}).fragment);
   }
 
   // If this partial is not yet registered, add it to a callback list to be called
   // when registered. When registered, replace the dummy node we created with the
   // rendered partial template.
-  var node = document.createTextNode('');
-  PARTIALS[path] || (PARTIALS[path] = []);
-  PARTIALS[path].push(function partialCallback(template){
-    if(!node.parentNode){ return void 0; }
-    (node.parentNode).replaceChild(render(template, env, scope, { contextualElement: renderNode}).fragment, node);
-  });
+  else{
+    PARTIALS[path] || (PARTIALS[path] = []);
+    PARTIALS[path].push(function partialCallback(template){
+      node.appendChild(render(template, env, scope, { contextualElement: renderNode}).fragment, node);
+    });
+  }
+
   return node;
+
 }
--- packages/rebound-htmlbars/test/rebound_helpers_partials_test.js
+++ packages/rebound-htmlbars/test/rebound_helpers_partials_test.js
@@ -43,40 +43,55 @@ QUnit.test('Rebound Partials', function( assert ) {
 
   var template, data, partial, dom = document.createDocumentFragment();
 
+  // Basic partial render
   template = compiler.compile('<div class={{bar}}>{{foo}}</div>', {name: 'test/partial'});
   partial = compiler.compile('{{partial "test/partial"}}');
   partial.render(dom, new Model({foo:'bar', bar:'foo'}));
-  equalTokens(dom, '<div class="foo">bar</div>', 'Partial helper inserts partial template');
+  equalTokens(dom, '<rebound-partial path="test/partial"><div class="foo">bar</div></rebound-partial>', 'Partial helper inserts partial template');
   assert.equal(document.querySelectorAll("script[src*='test/partial']").length, 0, "If a partial is already registered, compiling and running a template that consumes it does not attempt to fetch that partial from the server.");
 
 
+  // Partial data binding
   data = new Model({foo:'bar', bar:'foo', bool: false});
   partial.render(dom, data);
   data.set('foo', 'foo');
   data.set('bar', 'bar');
-  equalTokens(dom, '<div class="bar">foo</div>', 'Partial is data bound to data in scope');
+  equalTokens(dom, '<rebound-partial path="test/partial"><div class="bar">foo</div></rebound-partial>', 'Partial is data bound to data in scope');
 
+  // Dynamic, non-lazy partial
+  template = compiler.compile('<div class={{foo}}>{{bar}}</div>', {name: 'test/partial2'});
   partial = compiler.compile('{{partial partialName}}');
   data = new Model({partialName:'test/partial', foo:'bar', bar:'foo'});
   partial.render(dom, data);
-  equalTokens(dom, '<div class="foo">bar</div>', 'Partial can take a dynamic variable as partial name');
+  equalTokens(dom, '<rebound-partial path="test/partial"><div class="foo">bar</div></rebound-partial>', 'Partial can take a dynamic variable as partial name');
+  data.set('partialName', 'test/partial2');
+  equalTokens(dom, '<rebound-partial path="test/partial2"><div class="bar">foo</div></rebound-partial>', 'Partial can take a dynamic variable as partial name');
 
-
-
-  template = compiler.compile('<div class={{test.val}}>{{foo}}</div>', {name: 'test/partial2'});
-  partial = compiler.compile('{{#each local as |test|}}{{partial "test/partial2"}}{{/each}}', {name: 'test3'});
+  // Block scoped partial data
+  template = compiler.compile('<div class={{test.val}}>{{foo}}</div>', {name: 'test/partial3'});
+  partial = compiler.compile('{{#each local as |test|}}{{partial "test/partial3"}}{{/each}}', {name: 'test3'});
   data = new Model({foo:'bar', local:[{val: 'foo'}]});
   partial.render(dom, data);
-  equalTokens(dom, '<div class="foo">bar</div>', 'Partial has access to data in parents block scope');
+  equalTokens(dom, '<rebound-partial path="test/partial3"><div class="foo">bar</div></rebound-partial>', 'Partial has access to data in parents block scope');
   data.set('local[0].val','bar');
-  equalTokens(dom, '<div class="bar">bar</div>', 'Partial is data bound to data in block scope');
+  equalTokens(dom, '<rebound-partial path="test/partial3"><div class="bar">bar</div></rebound-partial>', 'Partial is data bound to data in block scope');
 
-
-
+  // Basic lazy partial
   (compiler.compile('{{partial "test/lazy-partial"}}')).render(dom, new Model({foo:'bar', bar:'foo'}));
-  equalTokens(dom, '', 'A Partial rendered before registration inserts an empty placeholder text node.');
+  equalTokens(dom, '<rebound-partial path="test/lazy-partial"></rebound-partial>', 'A Partial rendered before registration inserts an empty placeholder text node.');
   assert.equal(document.querySelectorAll("script[src*='test/lazy-partial']").length, 1, "If a partial is used that is not registered, compiling and running a template that consumes it will attempt to fetch that partial from the server.");
   compiler.compile('<div class={{bar}}>{{foo}}</div>', {name: 'test/lazy-partial'});
-  equalTokens(dom, '<div class="foo">bar</div>', 'After registration a pre-rendered Partial helper inserts the correct partial template');
+  equalTokens(dom, '<rebound-partial path="test/lazy-partial"><div class="foo">bar</div></rebound-partial>', 'After registration a pre-rendered Partial helper inserts the correct partial template');
+
+
+  // Dynamic lazy Partials
+  partial = compiler.compile('{{partial name}}', {name: 'dynamic-partial-test'});
+  data = new Model({name: 'test/dynamic-partial-1'});
+  partial.render(dom, data);
+  template = compiler.compile('Partial One', {name: 'test/dynamic-partial-1'});
+  equalTokens(dom, '<rebound-partial path="test/dynamic-partial-1">Partial One</rebound-partial>', 'Dynamic lazy partials work on first render');
+  data.set('name', 'test/dynamic-partial-2');
+  template = compiler.compile('Partial Two', {name: 'test/dynamic-partial-2'});
+  equalTokens(dom, '<rebound-partial path="test/dynamic-partial-2">Partial Two</rebound-partial>', 'Dynamic lazy partial names are databound');
 
 });
